9505a276f917c0fde0fcaed2dda52999e0b8e4cf
KimSeoJin <sjinjin7@gmail.com>
2022-07-29 12:39:57 +0900

    jpashop(JPA기본강의)

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    /*getter의 경우는 크게 신경 안 써도 되지만, Setter의 경우 유지보수성을 생가해서 신중하게 만드는 것이 좋다*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+}
