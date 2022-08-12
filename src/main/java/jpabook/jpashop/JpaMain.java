2556065dcc3d614584967c59da7509d2ecc54cc7
KimSeoJin <sjinjin7@gmail.com>
2022-08-12 20:26:12 +0900

    실전 예제 4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -21,8 +22,9 @@ public class JpaMain {
 
         try{
 
-            Order order = new Order ();
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
 
             tx.commit();
